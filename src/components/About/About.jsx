c571f4447c4c971d0664a01e359aa710b03323d6
Marsha Brown <marshabrown452@gmail.com>
2025-03-05 14:17:11 -0500

    update about section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -6,7 +6,12 @@ import { getImageUrl } from "../../utils";
 export const About = () => {
   return (
     <section className={styles.container} id="about">
-      <h2 className={styles.title}>About</h2>
+      <h2 className={styles.title}>About Me</h2>
+      <p className={styles.paragraph}>Hi, I’m Marsha, a passionate and driven front-end developer with a love for creating intuitive and visually appealing user interfaces. With a strong foundation in HTML, CSS, and JavaScript, I’m continuously building on my skills and diving deeper into the world of programming. I enjoy the challenge of translating complex ideas into sleek, user-friendly designs that make a real impact.
+
+When I’m not coding, you’ll find me exploring new trends in tech, or unwinding with anime and a good cup of coffee. I take pride in my work, always striving to learn and grow. Currently, I’m looking for opportunities where I can apply my knowledge, collaborate with creative teams, and continue improving as a developer.</p>
+
+<p className={styles.paragraph}>Feel free to check out my projects, and don’t hesitate to reach out if you’re interested in collaborating or have any questions!</p>
       <div className={styles.content}>
         <img
           src={getImageUrl("about/aboutImage.png")}
